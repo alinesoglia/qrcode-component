990f87e4e9d3a1fcafc741988f9838c3bdd0f0bc
alinesoglia <alinepsoglia@gmail.com>
2024-01-07 18:37:53 -0300

    final solution

---
+++ qrcodeChallenge/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { QrcodeComponent } from './qrcode/qrcode.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    QrcodeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
